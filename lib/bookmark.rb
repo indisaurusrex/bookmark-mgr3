d26df61e88c21d9f4f34725dc0fc65ea97560b63
India Rex <face940@gmail.com>
2020-06-30 16:29:37 +0100

    add pg gem and connect db

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,8 +1,9 @@
+require 'pg'
+
 class Bookmark
   def self.all
-    [
-      "http://www.makersacademy.com",
-      "http://www.google.com"
-     ]
+    connection = PG.connect(dbname: 'bookmark-manager') 
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 end

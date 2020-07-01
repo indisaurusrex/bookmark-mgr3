b6e84c3774198c85a2ad4fe8bcc4714763241b94
Katie Jones <katieljones29@gmail.com>
2020-07-01 16:33:53 +0100

    set up the test environment, if/else condition for connecting to test database for test purposes only

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -7,9 +7,12 @@ end
 
 feature "Viewing bookmarks" do
   scenario "A user is able to view bookmarks" do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
     visit ("/bookmarks")
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"
   end
 end
-

bc3f3d7bfa3ab12086b24e23f79fddfef73f78e4
India Rex <face940@gmail.com>
2020-07-01 15:55:49 +0100

    add test db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -112,4 +112,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+  config.before(:each) do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+    connection.exec("TRUNCATE bookmarks;")
+  end
 end
